bf911afd09aedf6e30d63ab5e90e899da2ac052a
lcs-ashin <ashin@lcs.on.ca>
2022-04-27 10:08:06 -0400

    Star moves left to right spinning.

--- SpinningStar/ContentView.swift
+++ SpinningStar/ContentView.swift
@@ -13,12 +13,17 @@ struct ContentView: View {
     // Starting position for the star (initial value)
     @State var xOffset: CGFloat = -100
     
+    // Control the rotation of the star
+    @State var starRotation = 0.0
+    
     // MARK: Computed properties
     var body: some View {
         Image(systemName: "star")
             .resizable()
             .frame(width: 40, height: 40)
             .foregroundColor(.purple)
+            .rotation3DEffect(Angle.degrees(starRotation),
+                          axis: (x: 0, y: 1, z: 0))
             .offset(x: xOffset, y: 0)
             .animation(
                 Animation
@@ -29,6 +34,8 @@ struct ContentView: View {
             .onTapGesture {
                 // 2. Logic that changes the state
                 xOffset = 100
+                // Spin the star twice
+                starRotation = 360 * 2
             }
     }
 }
